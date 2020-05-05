e0b883fecbe96ddb89ee37d89e3888f2bffb943d
sascha <sascha_lammers@gmx.de>
2020-05-04 20:31:18 -0700

    ESP32 pgm space header

--- scripts/templates/header.h
+++ scripts/templates/header.h
@@ -1,5 +1,9 @@
 #pragma once
+#if ESP32
+#include <pgmspace.h>
+#else
 #include <avr/pgmspace.h>
+#endif
 #ifndef PROGMEM_STRING_DECL
 #define PROGMEM_STRING_DECL(name)               extern const char _shared_progmem_string_##name[] PROGMEM;
 #define PROGMEM_STRING_DEF(name, value)         const char _shared_progmem_string_##name[] PROGMEM = { value };
