70113ec38d61869b362abddb95a2bd2297ea4668
sascha <sascha_lammers@gmx.de>
2021-03-29 06:37:12 -0700

    ...

---
+++ include/spgm_auto_strings.h
@@ -0,0 +1,33 @@
+// AUTO GENERATED FILE - DO NOT MODIFY
+#pragma once
+#include <spgm_string_generator.h>
+#ifdef __cplusplus
+extern "C" {
+#endif
+// SPGM:src/example.cpp:52:18, SPGM:src/example.cpp:53:18
+PROGMEM_STRING_DECL(Example3);
+// SPGM:src/example.cpp:54:18
+PROGMEM_STRING_DECL(build_flags);
+// SPGM:src/example.cpp:55:18
+PROGMEM_STRING_DECL(example_const_no_1);
+// SPGM:src/example.cpp:57:18
+PROGMEM_STRING_DECL(index_html);
+// SPGM:src/example.cpp:58:18, SPGM:src/example.cpp:61:18
+PROGMEM_STRING_DECL(New_string);
+// SPGM:src/example.cpp:59:18
+PROGMEM_STRING_DECL(New_string_2);
+// SPGM:src/example.cpp:60:18, SPGM:src/example.cpp:70:18, SPGM:src/example.cpp:74:18
+PROGMEM_STRING_DECL(New_string_3);
+// SPGM:src/example.cpp:62:18
+PROGMEM_STRING_DECL(degree_celsius_utf8);
+// SPGM:src/example.cpp:63:18
+PROGMEM_STRING_DECL(temperature);
+// SPGM:src/example.cpp:64:18
+PROGMEM_STRING_DECL(temperature_utf8);
+// SPGM:src/example.cpp:76:40, SPGM:src/example.cpp:77:18
+PROGMEM_STRING_DECL(CURRENCY);
+// SPGM:lib/test_lib/src/test_lib.cpp:7:18
+PROGMEM_STRING_DECL(test_class_output);
+#ifdef __cplusplus
+} // extern "C"
+#endif
---
+++ src/spgm_auto_strings.cpp
@@ -0,0 +1,26 @@
+// AUTO GENERATED FILE - DO NOT MODIFY
+#include "spgm_auto_strings.h"
+// SPGM:src/example.cpp:52:18, SPGM:src/example.cpp:53:18
+PROGMEM_STRING_DEF(Example3, "Inline Example 3");                                                                                                                // default
+// SPGM:src/example.cpp:54:18
+PROGMEM_STRING_DEF(build_flags, "Build Flags");                                                                                                                  // default
+// SPGM:src/example.cpp:55:18
+PROGMEM_STRING_DEF(example_const_no_1, "test \"escaped\" string");                                                                                               // default
+// SPGM:src/example.cpp:57:18
+PROGMEM_STRING_DEF(index_html, "index.html");                                                                                                                    // default
+// SPGM:src/example.cpp:58:18, SPGM:src/example.cpp:61:18
+PROGMEM_STRING_DEF(New_string, "New string");                                                                                                                    // default (auto)
+// SPGM:src/example.cpp:59:18
+PROGMEM_STRING_DEF(New_string_2, "New string 2");                                                                                                                // default (auto)
+// SPGM:src/example.cpp:60:18, SPGM:src/example.cpp:70:18, SPGM:src/example.cpp:74:18
+PROGMEM_STRING_DEF(New_string_3, "New string 3");                                                                                                                // default (auto)
+// SPGM:src/example.cpp:62:18
+PROGMEM_STRING_DEF(degree_celsius_utf8, "\xc2\xb0" "C\xc2\xb0" "F\xc2\xb0K");                                                                                    // default
+// SPGM:src/example.cpp:63:18
+PROGMEM_STRING_DEF(temperature, "23.5 \xc2\xb0" "C");                                                                                                            // default
+// SPGM:src/example.cpp:64:18
+PROGMEM_STRING_DEF(temperature_utf8, "23.5 \xc2\xb0" "C");                                                                                                       // default
+// SPGM:src/example.cpp:76:40, SPGM:src/example.cpp:77:18
+PROGMEM_STRING_DEF(CURRENCY, "%.2f");                                                                                                                            // default
+// SPGM:lib/test_lib/src/test_lib.cpp:7:18
+PROGMEM_STRING_DEF(test_class_output, "test class output");                                                                                                      // default
